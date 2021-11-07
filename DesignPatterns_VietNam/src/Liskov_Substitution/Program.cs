c559ba9e62b0e48d5f213d23a2c82b56e73fe69a
hoangtruong <hoangtruong@MSI>
2021-11-07 14:19:45 +0700

    Liskov_Substitution

---
+++ DesignPatterns_VietNam/src/Liskov_Substitution/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Liskov_Substitution
+{
+    public class Rectangle
+    {
+        public virtual int Width { get; set; }
+        public virtual int Height { get; set; }
+
+        public Rectangle()
+        {
+                
+        }
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height; 
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+        }
+    }
+
+    public class Square: Rectangle
+    {
+        public override int Width
+        {
+            set => base.Width = base.Height = value;
+        }
+        public override int Height
+        {
+            set => base.Width = base.Height = value;
+        }
+       
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
